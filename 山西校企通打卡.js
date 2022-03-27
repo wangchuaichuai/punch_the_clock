60ecd806b8af5cae4307eea9214a1a06c7d7cc50
NUC1913040228/NUC1913040228 <2750774003@qq.com>
2022-03-27 15:16:44 +0800

    提交自动打卡.js

--- 山西校企通打卡.js
+++ 山西校企通打卡.js
@@ -40,21 +40,43 @@ function task() {
   }
 }
 function unClock() {
+  if (device.sdkInt < 24) {
+    let root = true
+  } else {
+    let root = false
+  }
+
   if (!device.isScreenOn()) {
     device.wakeUp()
     sleep(500)
+
     swipe(500, 2000, 500, 1000, 210)
+    // log(text('返回').findOne())
+    // log(desc('返回').findOne())
     sleep(500)
-    let password = '19750102qwer'
-    input([0], password)
+    // let ttt = text('返回').findOne().bounds().click()
+    // click(ttt.centerX(), ttt.centerY())
+    // sleep(500)
+    let password = '159753'
+    数字锁屏
+    for (let i = 0; i < password.length; i++) {
+      sleep(500)
+      desc(password[i]).findOne().click()
+    }
+    // setText(password)
+    // desc('解锁').findOne().click()
+    // id('com.android.systemui:id/lock_icon').findOne().click()
+    // log('顶级的孩子', text('紧急呼叫').findOne().parent().parent().parent().child(0).childCount())
+    // log('顶级的孩子', text('紧急呼叫').findOne().parent().parent().parent().child(2).contentDescription)
+    // className("FrameLayout").parent().findOne().bounds().click(1070, 2300)
+    // let local = desc(m).findOne().bounds()
+    // click(local.centerX(), local.centerY()+50)
+
     sleep(500)
-    KeyCode("KEYCODE_ENTER")
-    log(KyeCode(66))
-    log(22222222222222222)
   }
 }
 auto()
-
+// let ra = new RootAutomator()
 unClock()
 
 launchApp('山西校企通')
